b2043f15971ac1a53189792621bea426b63f0df5
rohanvashisht <rohanprogrammer1@gmail.com>
2024-05-19 05:20:13 +0530

    ok

--- src/extension.js
+++ src/extension.js
@@ -2,24 +2,12 @@ const vscode = require('vscode');
 const path = require('path');
 
 function activate(context) {
-    // Register a command that executes the active python file in the interactive console
     let runBendFile = vscode.commands.registerCommand('runBendFile', function () {
-        // Get the active text editor
         let editor = vscode.window.activeTextEditor;
         if (editor) {
-            // Get the file name
-            let fileName = editor.document.fileName;
-            // Get the file base name without extension
-            let fileBaseName = path.basename(fileName, '.bend');
-            // Get the file directory
-            let fileDir = path.dirname(fileName);
-            // Save the file
             editor.document.save();
-            // Create a terminal
-            let terminal = vscode.window.createTerminal('Python Interactive');
-            // Send the command to the terminal
-            terminal.sendText(`bend run-c ${fileBaseName}.bend`);
-            // Show the terminal
+            let terminal = vscode.window.createTerminal('Bend Interactive');
+            terminal.sendText(`bend run-c "${editor.document.fileName}"`);
             terminal.show();
         }
     });
